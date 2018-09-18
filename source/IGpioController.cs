700052a4ff11c67036df219c7a3c03f9ae596cfe
Roy Salisbury <RoySalisbury@users.noreply.github.com>
2018-09-18 03:24:56 -0600

    Added interfaces for IGpioController and IGpioPin  (#28)

---
+++ source/IGpioController.cs
@@ -0,0 +1,46 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace Windows.Devices.Gpio
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IGpioController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        int PinCount
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pinNumber"></param>
+        /// <returns></returns>
+        IGpioPin OpenPin(int pinNumber);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pinNumber"></param>
+        /// <param name="sharingMode"></param>
+        /// <returns></returns>
+        IGpioPin OpenPin(int pinNumber, GpioSharingMode sharingMode);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pinNumber"></param>
+        /// <param name="sharingMode"></param>
+        /// <param name="pin"></param>
+        /// <param name="openStatus"></param>
+        /// <returns></returns>
+        bool TryOpenPin(int pinNumber, GpioSharingMode sharingMode, out IGpioPin pin, out GpioOpenStatus openStatus);
+    }
+}
