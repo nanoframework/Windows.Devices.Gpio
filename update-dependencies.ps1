164ac76c54a13548cca0f692a50aebbf852f5497
José Simões <josesimoes@users.noreply.github.com>
2018-10-15 15:57:53 +0100

    Update update-dependencies.ps1

--- update-dependencies.ps1
+++ update-dependencies.ps1
@@ -11,8 +11,8 @@ else
     # update dependencies for class libraries that depend on Windows.Devices.Gpio, mscorlib and others
 
     # because it can take sometime for the package to become available on the NuGet providers
-    # need to hange here for 5 minutes
-    Start-Sleep -Milliseconds 5000 * 60
+    # need to hange here for 5 minutes (5 * 60 * 1000)
+    Start-Sleep -Milliseconds 300000
 
     $librariesToUpdate =    ("lib-nanoFramework.Hardware.Esp32")
 
@@ -39,7 +39,7 @@ else
         $solutionFile = (Get-ChildItem -Path ".\" -Include "*.sln" -Recurse)
 
         # run NuKeeper inspect
-        if ($string -like '*release*' -or $string -like '*master*')
+        if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $env:APPVEYOR_REPO_BRANCH -like '*master*')
         {
             # use NuGet ONLY for release and master branches
             $nukeeperInspect = NuKeeper inspect --source https://api.nuget.org/v3/index.json
@@ -49,6 +49,9 @@ else
             # use NuGet and MyGet for all others
             $nukeeperInspect = NuKeeper inspect
         }
+
+        "NuGet update inspection result:" | Write-Host -ForegroundColor Blue
+        $nukeeperInspect | Write-Host -ForegroundColor White
 
         $packageCountMatch = [regex]::Match($nukeeperInspect, "Found (\d) possible updates").captures.groups[1].value
         [int]$packageCount = 0
@@ -61,7 +64,7 @@ else
             [array]$packageList = $packageListRaw -split [Environment]::NewLine
 
             # restore NuGet packages, need to do this before anything else
-            if ($string -like '*release*' -or $string -like '*master*')
+            if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $env:APPVEYOR_REPO_BRANCH -like '*master*')
             {
                 # use NuGet ONLY for release and master branches
                 nuget restore $solutionFile[0] -Source https://api.nuget.org/v3/index.json
@@ -93,7 +96,7 @@ else
                 $packageTargetVersion = $packageDetails.captures.Groups[6].Value.Trim();
     
                 # update package
-                if ($string -like '*release*' -or $string -like '*master*')
+                if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $string -like '*master*')
                 {
                     # use NuGet ONLY for release and master branches
                     $updatePackage = nuget update $solutionFile[0].FullName -Source https://api.nuget.org/v3/index.json
